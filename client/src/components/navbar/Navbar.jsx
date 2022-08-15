7e14ccd3fa5ea585d09ea6fa90a1e809756f7fc9
ys <ys.heo.korea@gmail.com>
2022-08-15 21:40:31 +0900

    client

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
+
+const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">lamabooking</span>
+        </Link>
+
+        {user ? user.email :  (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <Link to="/login">
+              <button className="navButton">Login</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

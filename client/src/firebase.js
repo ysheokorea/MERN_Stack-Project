444d47b2dc48d31b355b75156b6bc956b2103a1a
ys <ys.heo.korea@gmail.com>
2022-09-12 15:05:53 +0900

    client

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: "AIzaSyDPtgTQTfXyggTqK-9hcXvSPovR_qEXR6k",
+  apiKey: "",
   authDomain: "video-5177d.firebaseapp.com",
   projectId: "video-5177d",
   storageBucket: "video-5177d.appspot.com",
   messagingSenderId: "756420466614",
-  appId: "1:756420466614:web:4da4c3cbe1758219f05317",
-  measurementId: "G-F63J8MHMER"
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
